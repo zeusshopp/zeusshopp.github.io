22f48ca604632380ba1ab158577b3aa5e062fefd
zeusshopp <m0o1b2i3n4@gmail.com>
2024-11-05 20:33:13 +0330

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,24 @@
+function set(){
+    var user_name,result;
+    result = document.getElementById("user_name");
+    user_name = prompt("نام خود را وارد کنید");
+    if( user_name == null || user_name == "" ){
+     result.innerHTML ="بدون نام";
+    }
+    else{
+     localStorage.setItem("UserName", user_name);
+     get();
+    }
+}
+   
+function get(){
+    var result = document.getElementById("user_name");
+    var Uname = localStorage.getItem("UserName");
+    result.innerHTML = Uname;
+}
+
+function Del(){
+    localStorage.removeItem("user_name");
+   }
+
 const newLocal = document.getElementById("details").innerText = navigator.userAgent;
